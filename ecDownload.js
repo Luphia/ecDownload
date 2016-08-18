62f3131ea317dd59339b4a82eab97e96ef9ccfec
Luphia Chang <luphia@outlook.com>
2016-08-18 22:50:00 +0800

    Enhance: add retry mechanism

--- ecDownload.js
+++ ecDownload.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const https = require('https');
 const url = require('url');
 const path = require('path');
 const fs = require('fs');
@@ -8,11 +9,22 @@ var getFileNameFromURL = function (link) {
 	return path.parse(url.parse(link).pathname).base;
 };
 var ecDownload = function () {};
-ecDownload.download = function (link, savePath) {
-	if(typeof(savePath != 'string')) { savePath = getFileNameFromURL(link); }
+ecDownload.download = function (link, savePath, cb, retry) {
+	var self = this;
+	var options = url.parse(link);
+	var crawler = options.protocol == 'https:'? https: http;
+	options.rejectUnauthorized = false;
+	if(typeof(savePath) != 'string') { savePath = getFileNameFromURL(link); }
 	var file = fs.createWriteStream(savePath);
-	var request = http.get(link, function(response) {
+	var request = crawler.get(options, function(response) {
 		response.pipe(file);
+		cb = typeof(cb) == 'function'? cb: function () {};
+		response.on('end', cb);
+	});
+	request.on('error', function (e) {
+		retry = retry > 0? retry + 1: 1;
+		if(retry < 4 ) { setTimeout(function () { self.download(link, savePath, cb, retry); }, Math.random() * 3000); }
+		else { var e = new Error('failed ' + link); cb(e); }
 	});
 };
 
