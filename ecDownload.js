cf7bf2d2bee147cd3642ca55ea87283d62834930
Luphia Chang <luphia@outlook.com>
2018-08-24 02:31:08 +0800

    Update: promise version

--- ecDownload.js
+++ ecDownload.js
@@ -4,28 +4,40 @@ const url = require('url');
 const path = require('path');
 const fs = require('fs');
 
-var getFileNameFromURL = function (link) {
-	if(typeof(link) != 'string') { return ''; }
-	return path.parse(url.parse(link).pathname).base;
+const getFileNameFromURL = function (link) {
+  if(typeof(link) != 'string') { return ''; }
+  return path.parse(url.parse(link).pathname).base;
 };
-var ecDownload = function () {};
-ecDownload.download = function (link, savePath, cb, retry) {
-	var self = this;
-	var options = url.parse(link);
-	var crawler = options.protocol == 'https:'? https: http;
-	options.rejectUnauthorized = false;
-	if(typeof(savePath) != 'string') { savePath = getFileNameFromURL(link); }
-	var file = fs.createWriteStream(savePath);
-	var request = crawler.get(options, function(response) {
-		response.pipe(file);
-		cb = typeof(cb) == 'function'? cb: function () {};
-		response.on('end', cb);
+
+const ecDownload = function({ link, savePath, retry }) {
+  return new Promise((resolve, reject) => {
+    const retryTimes = retry > 0 ? retry : 0;
+    const options = url.parse(link);
+    const crawler = options.protocol == 'https:'? https: http;
+    let downloadTo = savePath;
+    options.rejectUnauthorized = false;
+    if(typeof(downloadTo) != 'string') {
+      downloadTo = getFileNameFromURL(link);
+    } else if(downloadTo.endsWith('/')) {
+      downloadTo = path.resolve(downloadTo, getFileNameFromURL(link));
+    }
+    const file = fs.createWriteStream(downloadTo);
+    const request = crawler.get(options, function(response) {
+      response.pipe(file);
+      response.on('end', resolve);
+    });
+    request.on('error', function (e) {
+      if(retry > 0 ) {
+      	console.log(retry);
+        setTimeout(() => {
+          ecDownload({ link, downloadTo, retry: (retry - 1) }).then(resolve, reject);
+        }, Math.random() * 3000);
+      } else {
+        const e = new Error('failed ' + link);
+        reject(e);
+      }
 	});
-	request.on('error', function (e) {
-		retry = retry > 0? retry + 1: 1;
-		if(retry < 4 ) { setTimeout(function () { self.download(link, savePath, cb, retry); }, Math.random() * 3000); }
-		else { var e = new Error('failed ' + link); cb(e); }
-	});
+  });
 };
 
 module.exports = ecDownload;
